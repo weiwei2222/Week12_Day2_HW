d982f047bb4287705d1129fe12217ab79c4b2cc5
Weiwei <linweiwei2222@163.com>
2023-12-06 18:27:25 -0500

    practice

--- server.js
+++ server.js
@@ -3,7 +3,27 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("test");
+  if ((req.params.number_of_bottles = 99)) {
+    res.send(
+      `${req.params.number_of_bottles} Bottles of beer on the wall.<a href=/${
+        req.params.number_of_bottles - 1
+      }>take one down, pass it around</a>`
+    );
+  }
+});
+
+app.get("/:number_of_bottles", function (req, res) {
+  if (req.params.number_of_bottles == 0) {
+    res.send(
+      `${req.params.number_of_bottles} Bottles of beer on the wall.<a href="/">start over</a>`
+    );
+  } else {
+    res.send(
+      `${req.params.number_of_bottles} Bottles of beer on the wall.<a href=/${
+        req.params.number_of_bottles - 1
+      }>take one down, pass it around</a>`
+    );
+  }
 });
 
 app.listen(port, () => {
